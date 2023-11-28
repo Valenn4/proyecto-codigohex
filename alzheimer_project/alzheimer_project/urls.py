30fb36208aa9cf3a7bf651bf41d866b6a98e3389
Carla <carlalema@MacBook-Air-de-Carla.local>
2023-11-28 13:20:35 +0100

    Estilos en el login y pagina sobre nosotros

--- alzheimer_project/alzheimer_project/urls.py
+++ alzheimer_project/alzheimer_project/urls.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
+from .views import about
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("authentication.urls")),
     path('', include('calender.urls')),
-    path('api/', include('api.urls'))
-
+    path('api/', include('api.urls')),
+    path('sobre_nosotros/',about)
 ]
+
+
