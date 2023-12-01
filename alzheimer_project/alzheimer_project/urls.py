be19a4880bb889d7c209edb5fc782f5cb5113906
Carla <carlalema@MacBook-Air-de-Carla.local>
2023-12-01 10:41:31 +0100

    Test

--- alzheimer_project/alzheimer_project/urls.py
+++ alzheimer_project/alzheimer_project/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import about
+from .views import about, profile
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -9,7 +9,9 @@ urlpatterns = [
     path('', include("authentication.urls")),
     path('', include('calender.urls')),
     path('api/', include('api.urls')),
-    path('sobre_nosotros/',about)
+    path('sobre_nosotros/',about,name='about'),
+    path('perfil/',profile,name='profile')
+    
 ]
 
 
